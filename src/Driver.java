4f6db2622f8960a22e941347a1cfb7d47c09c927
yasmine chiboub <ymchiboub@uncg.edu>
2024-09-06 16:51:42 -0400

    Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -3,12 +3,28 @@ public class Driver {
 
 	public static void main(String[] args) {
 
-		MyString s = new MyString("suMfd-=as@@1AAQmer");
-		MyString t = new MyString("T");
-		MyString u = new MyString(t);
+		MyString s = new MyString("hi");
+		MyString t = new MyString("there");
+		MyString u = new MyString(s);
 		
-		System.out.println(s.toLower() + " " + t + " " + u);
-		System.out.println(s.get(1));
+		System.out.println("length of s" + s.length());
+		System.out.println("character at index 1 in s: " + s.get(1));
+		System.out.println("s to upper: " + s.toUpper().toString());
+		System.out.println("t to lower: " + t.toLower().toString());
+
+		MyString d = s.concat(t);
+		System.out.println(d.toString());
+
+		System.out.println(s.indexOf(new MyString("i");
+
+		MyString e = new MyString("hiii");
+		System.out.println(e.lastIndexOf(new MyString("lo")));
+
+		System.out.println(s.substring(1).toString());
+                System.out.println(s.substring(1, 2).toString());
+
+		 System.out.println(s.compareTo(t)); 
+        	 System.out.println(s.compareTo(u)); 
 
 	}
 
